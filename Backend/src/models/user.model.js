9581d8c4b80d1a2f06a7e4578c1aa8dd78fcc4ee
SachinJamesA <143333095+SachinJamesA@users.noreply.github.com>
2024-12-09 11:00:41 +0530

    added password hashing

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,55 +1,102 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     phone: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     avatar: {
-        type: String, // cloudinary url
-        required: true,
+      type: String, // cloudinary url
+      required: true,
     },
     password: {
-        type: String,
-        required: [true, 'Pasword is required'],
+      type: String,
+      required: [true, "Pasword is required"],
     },
     refreshToken: {
-        type: String
+      type: String,
     },
     address: {
-        type: String
+      type: String,
     },
     // Fields for delivery person
     deliveryArea: {
-        type: String, 
+      type: String,
     },
     isAvailable: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password before saving to the database
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+      phone: this.phone,
+      address: this.address,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-}, {timestamps: true});
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
